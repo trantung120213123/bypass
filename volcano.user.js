b6f34f0c6aa58622f093c61a6fe2f62931ac74d6
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-11-07 11:28:22 +0700

    Update volcano.user.js

--- volcano.user.js
+++ volcano.user.js
@@ -3,13 +3,13 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.6.3.0
 // @description  You only need to solve the captcha
-// @author       DyRian (based on IHaxU)
+// @author       luex (based on IHaxU)
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @run-at       document-start
 // @grant        unsafeWindow
 // @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
-// @downloadURL  https://github.com/dyr14n/bypass/raw/main/volcano.user.js
-// @updateURL    https://github.com/dyr14n/bypass/raw/main/volcano.user.js
-// @require      https://raw.githubusercontent.com/trantung120213123/bypass/refs/heads/main/bypassvolcano.js
+// @downloadURL  https://github.com/trantung120213123/bypass/raw/main/volcano.user.js
+// @updateURL    https://github.com/trantung120213123/bypass/raw/main/volcano.user.js
+// @require      https://github.com/trantung120213123/bypass/raw/main/bypassvolcano.js
 // ==/UserScript==
