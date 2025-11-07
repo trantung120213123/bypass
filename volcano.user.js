85d528272c2e1157cb8ab6f158660bdbad485131
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-11-07 11:18:39 +0700

    Add files via upload

---
+++ volcano.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto Bypass Volcano
+// @namespace    http://tampermonkey.net/
+// @version      1.6.3.0
+// @description  You only need to solve the captcha
+// @author       DyRian (based on IHaxU)
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
+// @downloadURL  https://github.com/dyr14n/bypass/raw/main/volcano.user.js
+// @updateURL    https://github.com/dyr14n/bypass/raw/main/volcano.user.js
+// @require      https://github.com/dyr14n/bypass/raw/main/bypassvolcano.js
+// ==/UserScript==
